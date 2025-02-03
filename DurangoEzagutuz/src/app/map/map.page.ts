44a90ce77b8642e9ff46834f042e4cd88c1ba0e6
JAlberdi11 <alberdi.jon@uni.eus>
2025-02-03 11:06:43 +0100

    feature #18 : maparen logica eginda, geolokalizazioarena izan ezik estiloa ere eginda.

--- DurangoEzagutuz/src/app/map/map.page.ts
+++ DurangoEzagutuz/src/app/map/map.page.ts
@@ -1,32 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { IonicModule  } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
+import{ Location } from '../classes/location';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
   standalone: true,
-  imports: [IonicModule, RouterModule],
+  imports: [IonicModule, RouterModule, CommonModule],
 })
 export class MapPage implements OnInit {
+  // Lista con 5 objetos de la clase AppModels.Location (se asignan id, name e img)
+  locations: Location[] = [
+    { id: 1, name: 'Ubicacion 1', img: '../../assets/images/prueba.jpg' } as unknown as Location,
+    { id: 2, name: 'Ubicacion 2', img: '../../assets/images/prueba.jpg' } as unknown as Location,
+    { id: 3, name: 'Ubicacion 3', img: '../../assets/images/prueba.jpg' } as unknown as Location,
+    { id: 4, name: 'Ubicacion 4', img: '../../assets/images/prueba.jpg' } as unknown as Location,
+    { id: 5, name: 'Ubicacion 5', img: '../../assets/images/prueba.jpg' } as unknown as Location,
+  ];
+
+  // Propiedad para almacenar la ubicación seleccionada y mostrar la tarjeta
+  selectedLocation: Location | null = null;
+  selectedLocationId: number | null = null;
+  currentStop: number = 3;
   constructor() { }
 
   ngOnInit() {
-    const currentStop = 2; 
-
     document.querySelectorAll('.step').forEach((element) => {
       const img = element as HTMLImageElement;
-      const stepNumber = parseInt(img.dataset['step'] || '0', 10);
-      
-      console.log(stepNumber, currentStop);
-      
-      if (stepNumber <= currentStop) {
-          img.src = "../../assets/images/pasoLleno.png";
+      const step = img.dataset['step'];
+      const stepNumber = step ? parseInt(step, 10) : 0;
+  
+      console.log(stepNumber, this.currentStop);
+  
+      if (stepNumber <= this.currentStop) {
+        img.src = "../../assets/images/pasoLleno.png";
       } else {
-          img.src = "../../assets/images/paso.png";
+        img.src = "../../assets/images/paso.png";
       }
     });
+  }
+
+  // Abre la tarjeta buscando la ubicación según el id recibido
+  openCard(id: number): void {
+    this.selectedLocationId = id;
+    this.selectedLocation = this.locations.find(loc => loc.id === id) || null;
+  }
+
+  // Cierra la tarjeta
+  closeCard(): void {
+    this.selectedLocationId = null;
+    this.selectedLocation = null;
   }
 }
